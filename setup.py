8a05c2d3f01dfd710ffc972386a2ac435021fe3d
Sascha Mücke <sascha.muecke@tu-dortmund.de>
2022-02-21 15:16:46 +0100

    switched to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
         name='bitvec',
         packages=['bitvec'],
         version='0.1',
         description='Toolbox for binary vectors',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author='Sascha Muecke',
         author_email='sascha.muecke@tu-dortmund.de',
         url='https://github.com/smuecke/bitvec',
